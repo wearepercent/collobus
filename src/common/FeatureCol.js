a7b1ed24a94be7e22a28c62bfaf321fa0ab1f704
lauri3new <laurence@wearepercent.com>
2020-05-01 14:43:28 +0100

    ?

--- src/common/FeatureCol.js
+++ src/common/FeatureCol.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const FeatureCol = ({ src, title, description }) => (
-  <div style={{ backgroundColor: 'rgba(196, 201, 255, 1)', borderRadius: 50 }} className="featurescolumn col-12 col-md-4">
+  <div style={{
+    backgroundColor: 'rgba(196, 201, 255, 1)', borderRadius: 50, width: '80%',
+  }} className="featurescolumn col-12 col-md-4">
     <img src={src} />
     <h2>{title}</h2>
     <p>{description}</p>

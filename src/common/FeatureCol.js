7439f6739d2a4a080c960d14a19570e1d6e28ae1
james <james@wearepercent.com>
2020-05-01 15:35:10 +0100

    new folder for promo screen and alt warning fixes

--- src/common/FeatureCol.js
+++ src/common/FeatureCol.js
@@ -4,7 +4,7 @@ const FeatureCol = ({ src, title, description }) => (
   <div style={{
     backgroundColor: 'rgba(196, 201, 255, 1)', borderRadius: 50, width: '80%',
   }} className="featurescolumn col-12 col-md-4">
-    <img src={src} />
+    <img src={src} alt=''/>
     <h2>{title}</h2>
     <p>{description}</p>
   </div>
